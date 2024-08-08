94a60bf47eef89ca32294e000b175e838588cc6f
Agimir <william@mesaindigo.com>
2024-08-07 23:12:47 -0500

    Create shops.lua

---
+++ data/shops.lua
@@ -0,0 +1,57 @@
+Shop = {}
+
+Shop.PDM = {
+    blip = { label = '', spr = 1, clr = 1, scl = 0.6 },
+    label = '',
+    loc = {
+
+    }
+}
+
+Shop.LXM = {
+    blip = { label = '', spr = 1, clr = 1, scl = 0.6 },
+    label = '',
+    loc = {
+
+    }
+}
+
+Shop.BNS = {
+    blip = { label = '', spr = 1, clr = 1, scl = 0.6 },
+    label = '',
+    loc = {
+
+    }
+}
+
+Shop.MTO = {
+    blip = { label = '', spr = 1, clr = 1, scl = 0.6 },
+    label = '',
+    loc = {
+
+    }
+}
+
+Shop.BMX = {
+    blip = { label = '', spr = 1, clr = 1, scl = 0.6 },
+    label = '',
+    loc = {
+
+    }
+}
+
+Shop.AFM = {
+    blip = { label = '', spr = 1, clr = 1, scl = 0.6 },
+    label = '',
+    loc = {
+
+    }
+}
+
+Shop.MRN = {
+    blip = { label = '', spr = 1, clr = 1, scl = 0.6 },
+    label = '',
+    loc = {
+
+    }
+}
